3417889722dc3e196f473398fe980b4ed3759cc0
nhuallpa <huallpa.nestor@gmail.com>
2016-06-28 01:42:17 -0300

    Probando material for angular 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+  	MD_BUTTON_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MdToolbar,
+    MdButton
+  ],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Present for you';
 }
