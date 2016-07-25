6a46c55bf4815fd64b4c42f6c1dbe78b7f764103
myacobucci <maxyacobucci@gmail.com>
2016-07-25 01:10:37 -0300

    Subo preferencia funcionando con servicios

--- src/app/rubros/components/preferencia.ts
+++ src/app/rubros/components/preferencia.ts
@@ -2,4 +2,6 @@ export class Preferencia {
 	id: number;
 	name: string;
 	description: string;
+
+
 }
--- src/app/rubros/components/rubros.component.ts
+++ src/app/rubros/components/rubros.component.ts
@@ -7,6 +7,8 @@ import { RubrosService } from '../services/rubros.service';
 import { Preferencia } from './preferencia';
 import { UserSettingsService } from '../../user-settings.service';
 import { UserState }     from '../../core/user-state';
+import {Injectable} from '@angular/core';
+import { Router} from '@angular/router-deprecated';
 
 @Component({
   moduleId: module.id,
@@ -19,50 +21,75 @@ import { UserState }     from '../../core/user-state';
     MD_CHECKBOX_DIRECTIVES,
   ],
   providers: [RubrosService],
+
 })
+
+
 export class RubrosComponent implements OnInit {
+  title: string = 'Home Page';
+  body:  string = 'This is the about home body';
+  message: string;
   errorMessage: string;
   preferencias: Preferencia[];
   preferenciasPorUsuario: Preferencia[];
   isLogged:boolean;
+  algwo:boolean;
+  onee: Preferencia;
   userState:UserState;
+  selected: string[];
 
   constructor(private rubrosService:RubrosService,
-              private userSettingsService:UserSettingsService) {
+              private userSettingsService:UserSettingsService,
+              private router:Router) {
     this.isLogged = false;
   }
 
   ngOnInit() {
+    console.log("siiii");
+    this.rubrosService.getPreferenciasPorUsuario("1")
+                        .subscribe(
+                          preferenciasPorUsuario => this.preferenciasPorUsuario = preferenciasPorUsuario,
+                          error => this.errorMessage = <any>error);
     this.rubrosService.getPreferencias()
                         .subscribe(
                           preferencias => this.preferencias = preferencias,
                           error => this.errorMessage = <any>error);
-
-    this.userState = new UserState();
-    console.log("Registrado en app component");
-    this.userSettingsService.userStateObs$.subscribe(
-        userState => {
-          console.log("Recibido en Rubros");
-          this.userState = userState;
-          this.isLogged=userState.logged;
-          if (userState.logged) {
-            let idUser = userState.user.id;
-            this.cargarPreferencias(String(idUser));
-          } else {
-            this.preferenciasPorUsuario = [];
-          }
-        });
-    console.log(this.userSettingsService.userStateSource.observers);
+    this.selected = [];
+    }
     
+updateMessage(id:number, m: string): void {
+    console.log(this.selected.indexOf(id.toString()));
+    if (m){
+      if (this.selected.indexOf(id.toString()) < 0){
+        this.selected.push(id.toString());
+      }
+    } else{
+      if (this.selected.indexOf(id.toString()) >= 0){
+        var index = this.selected.indexOf(id.toString(), 0);
+        if (index > -1) {
+           this.selected.splice(index, 1);
+         }
+      }
+    }
+ }
 
+ savePreferences(): void{
+   this.rubrosService.deletePreferences()
+                     .subscribe(
+                          userState => this.updateState(userState),
+                          error =>  this.errorMessage = <any>error);;
+   for (var i = this.selected.length - 1; i >= 0; i--) {
+     this.rubrosService.saveOnePreference(this.selected[i])
+                           .subscribe(
+                          userState => this.updateState(userState),
+                          error =>  this.errorMessage = <any>error);;
+     console.log("se postea", this.selected[i]);
+   }
+ }
+
+ updateState(userState:UserState) {
+     
   }
 
-  cargarPreferencias(idUser:string) {
-    this.rubrosService.getPreferenciasByUser(idUser)
-                        .subscribe(
-                          preferencias => this.preferenciasPorUsuario = preferencias,
-                          error => this.errorMessage = <any>error);
-  }
-    
 
 }
